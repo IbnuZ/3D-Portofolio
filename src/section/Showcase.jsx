2d4e925b426fea22bab04f3a9d952bb87886c075
IbnuZ <ibnunasrulloh567@gmail.com>
2025-06-18 10:13:03 +0700

    fix showcase content

--- src/section/Showcase.jsx
+++ src/section/Showcase.jsx
@@ -49,16 +49,15 @@ const Showcase = () => {
         <div className="showcaselayout">
           <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/project1.png" alt="Ryde App Interface" />
+              <img src="/images/project1.jpeg" alt="Internship Management Platform" />
             </div>
             <div className="text-content">
               <h2>
-                On-Demand Rides Made Simple with a Powerful, User-Friendly App
-                called Ryde
+                Internship Management Made Easy with a Smart, User-Friendly App
               </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast,
-                user-friendly experience.
+                A web app built with React, Laravel, & TailwindCSS for efficient
+                internship tracking and coordination.
               </p>
             </div>
           </div>
@@ -67,18 +66,18 @@ const Showcase = () => {
             <div className="project" ref={libraryRef}>
               <div className="image-wrapper bg-[#FFEFDB]">
                 <img
-                  src="/images/project2.png"
-                  alt="Library Management Platform"
+                  src="/images/project2.jpeg"
+                  alt="Restaurant Management Platform"
                 />
               </div>
-              <h2>The Library Management Platform</h2>
+              <h2>Restaurant Management Platform</h2>
             </div>
 
             <div className="project" ref={ycDirectoryRef}>
-              <div className="image-wrapper bg-[#FFE7EB]">
-                <img src="/images/project3.png" alt="YC Directory App" />
+              <div className="image-wrapper bg-[#e0f1e0]">
+                <img src="/images/project3.jpeg" alt="YC Directory App" />
               </div>
-              <h2>YC Directory - A Startup Showcase App</h2>
+              <h2>Festival Music App</h2>
             </div>
           </div>
         </div>
