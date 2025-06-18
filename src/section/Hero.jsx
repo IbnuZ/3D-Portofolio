bb576bb58e7011d4432218a7d9c34087a0767870
IbnuZ <ibnunasrulloh567@gmail.com>
2025-06-18 15:48:14 +0700

    fix performance website

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -16,11 +16,7 @@ const Hero = () => {
 
   return (
     <section id="hero" className="relative overflow-hidden">
-      <div className="absolute top-0 left-0 z-10">
-        <img src="/images/bg.png" alt="" />
-      </div>
-
-      <div className="hero-layout">
+    <div className="hero-layout">
         {/* LEFT: Hero Content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
